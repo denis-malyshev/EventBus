4ff346ed5ae0afc50208b08b3c8b8ca06628d506
Denis Malyshev <denis.malyshev@teamdev.com>
2015-11-23 16:27:07 +0200

    Update eventBus test

--- ChatRoom/test/tests.js
+++ ChatRoom/test/tests.js
@@ -6,21 +6,20 @@ var chatRoomView;
 
 describe("Test app", function() {
 	before(function() {
+		console.log(new Date().toLocaleTimeString()+": START ALL TEST");
 		chat=new ChatRoom("234");
 		eb=new EventBus();
 		chatUserService=new ChatUserService(eb);
         chatRoomService=new ChatRoomService(chat, eb);
 		chatRoomView=new ChatRoomView(chat.id, eb);
 	});
-
-	beforeEach(function(done) {
-		setTimeout(function(){
-			done();
-		},100);
+	
+	after(function() {
+		console.log(new Date().toLocaleTimeString()+": THE END OF TESTS");
 	});
 	
 	it("check the initial state of the chat", function() {
-		unitjs.assert.equal(chat.messages.length,0);
+		unitjs.assert.equal(chat.messages.length,0);	
 	});
 	
 	it("check the state of the chat after adding first message", function() {
@@ -29,17 +28,23 @@ describe("Test app", function() {
 	});
 	
 	it("check of the chat contents", function() {
-		unitjs.assert.equal(chat.messages[0].message,"Hello");
+		unitjs.assert.equal(chat.messages[chat.messages.length-1].message,"Hello");
 	});
 	
-	it("adding message with eb", function() {
+	it("adding message with eb", function(done) {
 		eb.postMessage(chat.id, new Message("Vasya","hello",chat.id));
-		unitjs.assert.equal(chat.messages[1].message,"hello");
+		setTimeout(function() {
+			unitjs.assert.equal(chat.messages[chat.messages.length-1].message,"hello");
+			done();
+		},10);
 	});
 	
-	it("testing send message",function() {
+	it("testing send message",function(done) {
 		eb.postMessage("SEND", new Message("Masha","test text",chat.id));
-		unitjs.assert.equal(chat.messages.length,2);
+		setTimeout(function() {
+			unitjs.assert.equal(chat.messages.length,3);
+			done();
+		},10);			
 	});
 });
 
