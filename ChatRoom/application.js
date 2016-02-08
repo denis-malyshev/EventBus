47052954e283932804a502ce86e1dbc2b0170471
Denis Malyshev <denis.malyshev@teamdev.com>
2016-02-08 16:46:04 +0200

    Refactoring code

--- ChatRoom/application.js
+++ ChatRoom/application.js
@@ -1,20 +1,9 @@
-function app(/*users, chat, */eventBus) {
+function app(eventBus) {
 	
     return {
         "launch": function () {
 			new Controller(eventBus, new User(eventBus));
-			new View(eventBus).launch();
-			/*new renderRegistrationComp(eventBus);
-			new renderLoginComp(eventBus);*/
-			//new registrationComp(eventBus);
-			//new logout(eventBus);
-            /*new ChatRoomView(chat.id, eventBus);
-            new ChatRoomService(chat, eventBus);
-            for (var i = 0; i < users.length; i++) {
-				users[i].chatId=chat.id;
-                new ChatUserComponent("user" + Math.floor(Math.random() * 10e5) + i, users[i], eventBus);
-            }
-            new ChatRoomComponent(chat.id);*/
+			new View(eventBus).launch();			
         }
     };
 }
